28baf810eff0a86e6c0a5a44daa6f44acfe3070b
Pedro Fernandes <42358861+PedroFernandesSpark@users.noreply.github.com>
2019-09-24 13:49:12 -0300

    Add files via upload

---
+++ shibarides-master/shibarides/shibarides/ShibaRidesDomains.cpp
@@ -0,0 +1,65 @@
+#include "ShibaRidesDomains.hpp"
+#include <iostream>
+#include <string>
+using namespace shibarides;
+
+Dominio::Dominio(){
+    this->valueSet = false;
+    this->value = "";
+}
+
+
+void Dominio::setValue(std::string value) throw (std::invalid_argument){
+    this->validate(value);
+    this->value = value;
+    this->valueSet = true;
+}
+
+std::string Dominio::getValue() const throw (std::exception){
+    if(this->valueSet) return this->value;
+    else throw std::exception();
+}
+
+void Dominio::validate(std::string value) throw (std::invalid_argument){}
+
+void Assento::validate(std::string value) throw (std::invalid_argument){
+    if(value!="D" && value!="T") throw (std::invalid_argument("Argument must be D or T"));
+}
+
+void Duracao::validate(std::string value) throw (std::invalid_argument){
+    int tamanho_string = 0;
+    for (tamanho_string = 0;tamanho_string < value.length();tamanho_string++){
+        if(value[tamanho_string]>57 || value[tamanho_string]<48)
+            throw (std::invalid_argument("Argument must be a inter"));
+        if(value>48)
+            throw (std::invalid_argument("Argument must be less than 48"));
+    }
+}
+
+void Vagas::validate(std::string value) throw (std::invalid_argument){
+    int tamanho_string = 0;
+    if (value.length()>1) throw (std::invalid_argument("Argument must be from 0 to 4")){
+    for (tamanho_string = 0;tamanho_string < value.length();tamanho_string++){
+        if(value[tamanho_string] < 48 || value[tamanho_string] > 52)
+            throw (std::invalid_argument("Argument must be from 0 to 4"));
+    }
+    }
+}
+
+void Bagagem::validate(std::string value) throw (std::invalid_argument){
+    int tamanho_string = 0;
+    if (value.length()>1) throw (std::invalid_argument("Argument must be from 0 to 4")){
+    for (tamanho_string = 0;tamanho_string < value.length();tamanho_string++){
+        if(value[tamanho_string] < 48 || value[tamanho_string] > 52)
+            throw (std::invalid_argument("Argument must be from 0 to 4"));
+    }
+    }
+}
+
+void Estado::validate(std::string value) throw (std::invalid_argument){
+    int tamanho_string = 0;
+    if (value!=AC||value!=AL||value!=AP||value!=AM||value!=BA||value!=CE||value!=DF||
+        value!=ES||value!=GO||value!=MA||value!=MT||value!=MS||value!=MG||value!=PA||value!=PB||
+        value!=PR||value!=PE||value!=PI||value!=RJ||value!=RN||value!=RS||value!=RO||value!=RR||
+        value!=SC||value!=SP||value!=SE||value!=TO) throw (std::invalid_argument("Argument inst a valid state"))
+}
---
+++ shibarides-master/shibarides/shibarides/ShibaRidesDomainsUT.cpp
@@ -0,0 +1,146 @@
+#include "ShibaRidesDomainsUT.hpp"
+using namespace shibarides;
+
+TUDominio::TUDominio(){
+    this->successValues.clear();
+    this->failValues.clear();
+    this->genSuccessValues();
+    this->genFailValues();
+}
+
+int TUDominio::run(){
+    this->createDomain();
+    this->successCase();
+    this->failCase();
+    this->destroyDomain();
+    return this->state;
+}
+
+void TUDominio::genFailValues(){};
+void TUDominio::genSuccessValues(){};
+
+void TUDominio::createDomain(){};
+
+void TUDominio::successCase(){
+    std::vector<std::string>::iterator ptr;
+
+    for(ptr = this->successValues.begin(); ptr<this->successValues.end(); ptr++){
+        try{
+            this->domain->setValue(*ptr);
+            if(this->domain->getValue() != *ptr) this->state = TUDominio::FAIL;
+        }catch(std::exception e){
+            this->state = TUDominio::FAIL;
+        }
+    }
+}
+
+void TUDominio::failCase(){
+     std::vector<std::string>::iterator ptr;
+
+    for(ptr = this->failValues.begin(); ptr<this->failValues.end(); ptr++){
+        try{
+            this->domain->setValue(*ptr);
+            this->state = TUDominio::FAIL;
+        }catch(std::exception e){
+            return;
+        }
+    }
+}
+
+
+void TUDominio::destroyDomain(){
+    delete this->domain;
+}
+
+// =========================================================================
+
+void TUAssento::createDomain(){
+    this->domain = new Assento();
+    this->state = TUDominio::SUCCESS;
+}
+
+void TUAssento::genFailValues(){
+    this->failValues.push_back("A");
+    this->failValues.push_back("B");
+    this->failValues.push_back("C");
+}
+
+void TUAssento::genSuccessValues(){
+    this->failValues.push_back("D");
+    this->failValues.push_back("T");
+}
+
+//============================================================================
+void TUDuracao::createDomain(){
+    this->domain = new Duracao();
+    this->state = TUDominio::SUCCESS;
+}
+
+void TUDuracao::genFailValues(){
+    this->failValues.push_back("asdaw");
+    this->failValues.push_back("5/*+-");
+    this->failValues.push_back("     ");
+    this->failValues.push_back("02.13");
+}
+
+void TUDuracao::genSuccessValues(){
+    this->failValues.push_back("123");
+    this->failValues.push_back("03");
+}
+
+//============================================================================
+void TUVagas::createDomain(){
+    this->domain = new Vagas();
+    this->state = TUDominio::SUCCESS;
+}
+
+void TUVagas::genFailValues(){
+    this->failValues.push_back("qweas");
+    this->failValues.push_back("5/*+-");
+    this->failValues.push_back("     ");
+    this->failValues.push_back("02.13");
+    this->failValues.push_back("5");
+}
+
+void TUVagas::genSuccessValues(){
+    this->failValues.push_back("1");
+    this->failValues.push_back("0");
+}
+//============================================================================
+void TUBagagem::createDomain(){
+    this->domain = new Bagagem();
+    this->state = TUDominio::SUCCESS;
+}
+
+void TUBagagem::genFailValues(){
+    this->failValues.push_back("qweas");
+    this->failValues.push_back("5/*+-");
+    this->failValues.push_back("     ");
+    this->failValues.push_back("02.13");
+    this->failValues.push_back("5");
+}
+
+void TUBagagem::genSuccessValues(){
+    this->failValues.push_back("1");
+    this->failValues.push_back("0");
+}
+
+//============================================================================
+
+void TUBagagem::createDomain(){
+    this->domain = new Bagagem();
+    this->state = TUDominio::SUCCESS;
+}
+
+void TUBagagem::genFailValues(){
+    this->failValues.push_back("qweas");
+    this->failValues.push_back("5/*+-");
+    this->failValues.push_back("     ");
+    this->failValues.push_back("02.13");
+    this->failValues.push_back("5");
+}
+
+void TUBagagem::genSuccessValues(){
+    this->failValues.push_back("DF");
+    this->failValues.push_back("MG");
+}
